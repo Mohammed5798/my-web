0c0ce051f4530384da86da9e0cf4562587b0ffc9
unknown <mohammedrich000@gmail.com>
2024-12-19 16:16:35 +0300

    second commit navbar

--- myweb/src/components/navbar.tsx
+++ myweb/src/components/navbar.tsx
@@ -1,10 +1,32 @@
 import '../style/main.css';
+import { LuSunMedium } from "react-icons/lu";
 
 function NavBar() {
     return ( 
         <div className="navbar-container">
 
-            <img src="../../public/assets/logo.svg" alt="" />
+            <div className="main-navbar">
+
+                <img className='logo' src="/assets/logo.svg" alt="hi" />
+
+                <div className="middle">
+                    
+                    <span className="left-line"> <span className="circle1"></span> </span>
+
+                    <h1 className="menu">menu</h1>
+                    
+                    <span className="right-line"> <span className="circle2"></span> </span>
+
+                </div>
+
+                <span className="sun"><LuSunMedium /></span>
+
+            </div>
+
+            {/* <div className="full-navbar">
+
+            </div> */}
+
 
         </div>
      );
