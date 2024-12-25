27da1d845d33670319d099d3ae5bae0f5e5d5d76
unknown <mohammedrich000@gmail.com>
2024-12-26 01:28:57 +0300

    third commit navbar

--- myweb/src/App.tsx
+++ myweb/src/App.tsx
@@ -1,12 +1,30 @@
 import './style/main.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import NavBar from './components/navbar';
+import HomePage from './pages/home';
+import GraphicDesign from './pages/graphicDesign';
+import WebDev from './pages/webDev';
+import Contact from './pages/contact';
+import AboutMe from './pages/aboutMe';
 
 function App() {
 
   return (
 
+
     <div className="main">
+
+    <Router>
       <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/aboutMe' element={<AboutMe/>}/>
+          <Route path='/graphicDesign' element={<GraphicDesign/>}/>
+          <Route path='/webDev' element={<WebDev/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </Router>
+
     </div>
     
   )
--- myweb/src/components/navbar.tsx
+++ myweb/src/components/navbar.tsx
@@ -1,35 +1,87 @@
 import '../style/main.css';
 import { LuSunMedium } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
-    return ( 
+    const [isActive, setIsActive] = useState(true);
+
+    const menuClicking = () => {
+        setIsActive(!isActive);
+    };
+
+    // const menuClose = ()=>{ setIsActive(isActive); console.log("hi")};
+
+    const downloadingCV = ()=>{};
+
+    return (
+
         <div className="navbar-container">
 
-            <div className="main-navbar">
+            <div className={isActive ? "inactive-navbar" : "active-navbar"}>
 
                 <img className='logo' src="/assets/logo.svg" alt="hi" />
 
                 <div className="middle">
-                    
+
                     <span className="left-line"> <span className="circle1"></span> </span>
 
-                    <h1 className="menu">menu</h1>
-                    
+                    <h1 className="menu" onClick={menuClicking}
+
+                    >menu</h1>
+
                     <span className="right-line"> <span className="circle2"></span> </span>
 
                 </div>
 
-                <span className="sun"><LuSunMedium /></span>
+                <span className="span-sun"><LuSunMedium className='sun' /></span>
+
+                {/* active navbar */}
+                <div className="top">
+                    <h1 className='button-close' onClick={menuClicking}><IoMdClose /></h1>
+                </div>
+
+                <div className="down">
+
+                    <div className="left">
+
+                        <Link to="/">home</Link>
+
+                        <Link to="/aboutMe">about me</Link>
+
+                        <label htmlFor="options">my skills</label>
+
+                        <select id="options">
+
+                            <option value="option1">graphic design</option>
+
+                            <option value="option2">web development</option>
+
+                        </select>
+                        
+                        <Link to="/contact" className='link'>contact</Link>
+
+                        {/* not completed */}
+                        <p className="CV" onClick={downloadingCV}>download CV</p>
+
+                    </div>
+
+                    <div className="right">
+                        <img src="/assets/Ellipse 9.svg" alt="" />
+
+                        <div className="icons">
+                            <div className="left"></div>
+                            <div className="right"></div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
 
-            {/* <div className="full-navbar">
-
-            </div> */}
-
-
         </div>
-     );
+    );
 };
 
 export default NavBar;
